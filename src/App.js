223227d59771cecadfbfc0e35ee8248d922e233e
Nagy Robert <robert.cristian@me.com>
2019-05-03 17:33:05 +0300

    Added Show Previous Location Weather Info

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-const API_KEY = "20d1ffdff3cea23d7f78edf09b748a82";
+const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
 
 class App extends React.Component {
     state = {
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: undefined
+        current: {
+            temperature: undefined,
+            city: undefined,
+            country: undefined,
+            humidity: undefined,
+            description: undefined,
+            error: undefined
+        },
+        previous: {
+            temperature: undefined,
+            city: undefined,
+            country: undefined,
+            humidity: undefined,
+            description: undefined,
+            error: undefined
+        },
+        searchCounter: 0,
+        showPrevious: false
     };
 
     getWeather = async (e) => {
@@ -20,27 +32,46 @@ class App extends React.Component {
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
 
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+
         const data = await api_call.json();
+
         if (city && country) {
+            if (this.state.searchCounter > 0 && this.state.current.city != city) {
+                this.setState({
+                    previous: this.state.current
+                })
+            }
             this.setState({
-                temperature: data.main.temp,
-                city: data.name,
-                country: data.sys.country,
-                humidity: data.main.humidity,
-                description: data.weather[0].description,
-                error: ""
+                current: {
+                    temperature: data.main.temp,
+                    city: data.name,
+                    country: data.sys.country,
+                    humidity: data.main.humidity,
+                    description: data.weather[0].description,
+                    error: ""
+                },
+                searchCounter: this.state.searchCounter + 1,
+                showPrevious: false
             })
         } else {
             this.setState({
-                temperature: undefined,
-                city: undefined,
-                country: undefined,
-                humidity: undefined,
-                description: undefined,
-                error: "Please Enter the value"
+                current: {
+                    temperature: undefined,
+                    city: undefined,
+                    country: undefined,
+                    humidity: undefined,
+                    description: undefined,
+                    error: "Please Enter the value"
+                }
             })
         }
+    };
+
+    handlePreviousBtnClick = () => {
+        this.setState({
+            showPrevious: true
+        })
     };
 
     render() {
@@ -55,14 +86,14 @@ class App extends React.Component {
                                 </div>
 
                                 <div className="col-7 form-container">
-                                    <Form getWeather={this.getWeather}/>
+                                    <Form getWeather={this.getWeather} showPrevious={this.handlePreviousBtnClick}/>
                                     <Weather
-                                        temperature={this.state.temperature}
-                                        city={this.state.city}
-                                        country={this.state.country}
-                                        humidity={this.state.humidity}
-                                        description={this.state.description}
-                                        error={this.state.error}
+                                        temperature={this.state.showPrevious ? this.state.previous.temperature : this.state.current.temperature}
+                                        city={this.state.showPrevious ? this.state.previous.city : this.state.current.city}
+                                        country={this.state.showPrevious ? this.state.previous.country : this.state.current.country}
+                                        humidity={this.state.showPrevious ? this.state.previous.humidity : this.state.current.humidity}
+                                        description={this.state.showPrevious ? this.state.previous.description : this.state.current.description}
+                                        error={this.state.current.error}
                                     />
                                 </div>
                             </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Form = props => (
-    <form onSubmit={props.getWeather}>
+    <form className="flex" onSubmit={props.getWeather}>
         <input type="text" name="city" placeholder="City"/>
         <input type="text" name="country" placeholder="Country"/>
         <button>Search</button>
+        <button type="reset" onClick={props.showPrevious}>Previous</button>
     </form>
 );
 
